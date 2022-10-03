de057dfd59bcb07378d7cbc07c8cb5701399154e
MasterGordon <gologo.x@gmail.com>
2022-10-03 23:20:31 +0200

    update window title

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var window = new Window("Hello World!", 800, 600);
+var window = new Window("ASDLteroids", 800, 600);
 var renderer = new Renderer(window);
 var scene = new Scene(renderer);
 scene.Run();
